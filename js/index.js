c9e4ce65e93a5a0761b082411fe47afa416592db
Joyce Jiang <jj3115@barnard.edu>
2021-01-07 13:12:56 -0500

    day 4

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+	console.log("hello")
+
+	$('.animoji').on('onclick', function() {
+   		$(this).html('<img src="images/animoji hover.png" height=220px>');
+  	});
+});
+
