f3781dc28872884cec30bad7d1fa2675c2d55604
Joyce Jiang <76878583+joycexjiang@users.noreply.github.com>
2021-10-24 16:22:35 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+	console.log("hello")
+
+	$('.animoji').on('onclick', function() {
+   		$(this).html('<img src="images/animoji hover.png" height=220px>');
+  	});
+});
